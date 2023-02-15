464f3451822ab0f701325a036e56817093a48411
gyorgynorbert <94974450+gyorgynorbert@users.noreply.github.com>
2023-02-15 18:47:03 +0200

    Update hamburger.js

--- Scripts/hamburger.js
+++ Scripts/hamburger.js
@@ -1,14 +1,40 @@
 const hamburger = document.querySelector(".hamburger");
+const hamburgerLines = document.querySelectorAll(".ham-line");
 const navLinks = document.querySelector(".navbar-right");
-const links = document.querySelectorAll(".navbar-right > a");
+const links = document.querySelectorAll(".navbar-item");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
-  //Animate Links
-  navLinks.classList.toggle("open");
+  navLinks.classList.toggle("open"); 
+
+  hamburger.classList.toggle("toggle");
+
+  body.classList.toggle("freeze");
+
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
 
-  //Hamburger Animation
-  hamburger.classList.toggle("toggle");
+  hamburgerLines.forEach((lines) => {
+    lines.classList.toggle("fading");
+  });
 });
+
+document.querySelectorAll(".navbar-item").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("toggle");
+
+  navLinks.classList.remove("open");
+  
+  body.classList.remove("freeze");
+  
+  hamburgerLines.forEach((lines) => {
+    lines.classList.remove("fading");
+  });
+  
+  links.forEach((link) => {
+    link.classList.remove("fade");
+  });
+}))
+
+
+
